09fa7073dca37871bcaf576a782bbcca59b529f6
Ramon181 <102682605+Ramon181@users.noreply.github.com>
2022-09-27 17:36:35 -0500

    Barra de Navegacion y Barra de Busqueda

--- src/components/user/searchBar/searchBar.jsx
+++ src/components/user/searchBar/searchBar.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import './searchBar.css'
 
 
 const SearchBar = () => {
-    return ( 
-        <div>
-            <input type='search' placeholder='Search'/>
-            <button >Search</button>
+    return (
+        <div class="inpur_body">
+            <div class="input-group">
+                <input type="text" class="input" id="Email" name="Email" placeholder="Search..." autocomplete="off" />
+                <input class="button--submit" value="Search" type="submit" />
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default SearchBar;
