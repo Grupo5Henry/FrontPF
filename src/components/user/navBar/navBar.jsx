1a4139f295294450599d0d0ee5a967e17dce2a9d
gtheaded <gaston.turner@gmail.com>
2022-10-02 16:46:04 -0300

    Ahora el corazón de favoritos se muestra si estamos logueados

--- src/components/user/navBar/navBar.jsx
+++ src/components/user/navBar/navBar.jsx
@@ -137,13 +137,15 @@ const NavBar = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
               </svg>
             </Link>
-            <div className="flex items-center relative mr-5">
-              <Link to={'/favorites'} className="hover:text-gray-200">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+            {
+             userStatus && (<div class="flex items-center relative mr-5">
+              <Link to={'/favorites'} class="hover:text-gray-200">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                 </svg>
               </Link>
-            </div>
+            </div>)
+            }
             <div className="dropdown relative mr-5">
               <a
                 className="dropdown-toggle flex items-center hidden-arrow"
