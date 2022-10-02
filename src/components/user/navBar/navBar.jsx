722daacf94fc8d81cb7d17b6bc66e8ce96146305
gtheaded <gaston.turner@gmail.com>
2022-10-02 16:43:55 -0300

    trabajando en favoritos del navbar

--- src/components/user/navBar/navBar.jsx
+++ src/components/user/navBar/navBar.jsx
@@ -115,15 +115,13 @@ const NavBar = () => {
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
               </svg>
             </Link>
-            {
-             userStatus && (<div class="flex items-center relative mr-5">
+            <div class="flex items-center relative mr-5">
               <Link to={'/favorites'} class="hover:text-gray-200">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                 </svg>
               </Link>
-            </div>)
-            }
+            </div>
             <div class="dropdown relative mr-5">
               <a
                 class="dropdown-toggle flex items-center hidden-arrow"
