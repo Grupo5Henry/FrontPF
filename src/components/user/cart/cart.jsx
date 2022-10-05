f201b678f429914c72b5e9bb35153373c0925940
Gonzalo <zapozapico@gmail.com>
2022-10-05 17:21:42 -0300

    Cart button implementation in favorites and detail

--- src/components/user/cart/cart.jsx
+++ src/components/user/cart/cart.jsx
@@ -1,16 +1,13 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
-import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { AddShoppingCart, Favorite } from "@mui/icons-material";
-import accounting from "accounting";
+import { AddShoppingCart, Favorite, FavoriteBorder } from "@mui/icons-material";
 import { useDispatch, useSelector } from "react-redux";
-import { getCart } from "../../../redux/action";
-import axios from "axios";
+import { getCart, getFavorites } from "../../../redux/action";
+import { isFavorite, setFavorite, unSetFavorite } from "../../../Controllers/Favorite";
+import { addToCart, inCart, updateCart, updateOfflineCart } from "../../../Controllers/Cart";
+import { NumberInput, NumberInputField, NumberInputStepper, NumberDecrementStepper, NumberIncrementStepper } from "@chakra-ui/react";
 
 
 
@@ -29,14 +26,15 @@ const ExpandMore = styled((props) => {
 
 function Cart () {
   const [expanded, setExpanded] = React.useState(false);
-  const [inCart, setInCart] = React.useState(1)
   const cart = useSelector(state => state.cart);
-  const userStatus = useSelector(state => state.loggedIn);
+  const userState = useSelector(state => state.loggedIn);
+  const favorites = useSelector(state => state.favorites)
   const dispatch = useDispatch()
 
 
   React.useEffect(() => {
     // console.log(userStatus, localStorage.userName)
+    dispatch(getFavorites(localStorage.userName))
     dispatch(getCart(localStorage.userName))
   },[])
 
@@ -50,51 +48,86 @@ function Cart () {
         {cart !=="Missing Username" && cart.map((product) => {
           let detail = product.product 
           return (
-          <div key={`cart${detail.id}`} className="group group-hover:bg-opacity-60 transition duration-500 relative bg-gray-50 sm:p-28 py-36 px-10 flex justify-center items-center">
-            <img
-              className="group-hover:opacity-60 transition duration-500"
-              src={detail.thumbnail}
-              alt="sofa-2"
-            />
-            <div className="absolute sm:top-8 top-4 left-4 sm:left-8 flex justify-start items-start flex-col space-y-2">
-              <div>
-                <p className="group-hover:opacity-60 transition duration-500 text-xl leading-5 text-gray-600">
-                  {detail.name}
-                </p>
-                <p className="group-hover:opacity-60 transition duration-500 text-xl leading-5 text-gray-600">
-                  {detail.condition}
-                </p>
+            <a key={`cart${detail.id}`} href="#" className="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img className="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={detail.thumbnail} alt="" />
+              <div className="flex flex-col justify-between p-4 leading-normal">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{detail.name}</h5>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Marca: {detail.brand}</p>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Modelo: {detail.model}</p>
               </div>
-              <div>
-                <p className="group-hover:opacity-60 transition duration-500 text-xl font-semibold leading-5 text-gray-800">
-                {accounting.formatMoney(detail.price, "$")}
-                </p>
-                <p className="group-hover:opacity-60 transition duration-500 text-xl leading-5 text-gray-600">
-                  {detail.brand}
-                </p>
-              </div>
-            </div>
+              <CardActions disableSpacing >
+              <IconButton aria-label="Add to cart" onClick={() => {
+                if (isFavorite(detail.id)) return unSetFavorite(localStorage.userName, detail.id)
+                setFavorite(localStorage.userName, detail.id)
+              }
+              }>
+                {favorites != "Missing Username" ? (isFavorite(detail.id) 
 
-            <div className="flex flex-col bottom-8 left-8 space-y-4 absolute opacity-0 group-hover:opacity-100 transition duration-500">
-              <CardActions disableSpacing>
-                <ExpandMore
-                  expand={expanded}
-                  onClick={handleExpandClick}
-                  aria-expanded={expanded}
-                  aria-label="show more"
-                >
-                  <ExpandMoreIcon />
-                </ExpandMore>
-              </CardActions>
-              <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                <Typography paragraph>{detail.model}</Typography>
+                ? <Favorite sx={{ color: "red"}} fontSize="large" 
+                // onClick={() => unSetFavorite(localStorage.userName, product.id)}
+                /> 
 
-                  <Typography paragraph>{detail.description}</Typography>
-                </CardContent>
-              </Collapse>
-            </div>
-          </div>
+                : <FavoriteBorder fontSize="large"
+                />) : null}
+                  
+                </IconButton>
+                <NumberInput size='sm'  defaultValue={product.amount} min={0} onChange={
+                  (value) =>{ 
+                   if (userState) {
+                    updateCart(localStorage.userName, detail.id, value)
+                    return
+                  }
+                  updateOfflineCart(detail.id, value)
+                  }}>
+                  <NumberInputField focusBorderColor='red.200' />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper
+                      bg='green.200'
+                      _active={{ bg: 'green.300' }}
+                      children='+'
+                    />
+                    <NumberDecrementStepper
+                      bg='pink.200'
+                      _active={{ bg: 'pink.300' }}
+                      children='-'
+                    />
+                  </NumberInputStepper>
+                </NumberInput>
+                </CardActions>
+          </a>
+
+
+
+
+          // <div key={`cart${detail.id}`} className="group group-hover:bg-opacity-60 transition duration-500 relative bg-gray-50 sm:p-28 py-36 px-10 flex justify-center items-center">
+          //   <img
+          //     className="group-hover:opacity-60 transition duration-500"
+          //     src={detail.thumbnail}
+          //     alt="sofa-2"
+          //   />
+          //   <div className="absolute sm:top-8 top-4 left-4 sm:left-8 flex justify-start items-start flex-col space-y-2">
+          //     <div>
+          //       <p className="group-hover:opacity-60 transition duration-500 text-xl leading-5 text-gray-600">
+          //         {detail.name}
+          //       </p>
+          //       <p className="group-hover:opacity-60 transition duration-500 text-xl leading-5 text-gray-600">
+          //         {detail.condition}
+          //       </p>
+          //     </div>
+          //     <div>
+          //       <p className="group-hover:opacity-60 transition duration-500 text-xl font-semibold leading-5 text-gray-800">
+          //       {accounting.formatMoney(detail.price, "$")}
+          //       </p>
+          //       <p className="group-hover:opacity-60 transition duration-500 text-xl leading-5 text-gray-600">
+          //         {detail.brand}
+          //       </p>
+          //     </div>
+          //   </div>
+
+          //   <div className="flex flex-col bottom-8 left-8 space-y-4 absolute opacity-0 group-hover:opacity-100 transition duration-500">
+              
+          //   </div>
+          // </div>
         )})}
       </div>
     </div>
