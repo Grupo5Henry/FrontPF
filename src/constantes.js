ea4e7be54008446b1edee7abfda8631fdbb17168
gtheaded <gaston.turner@gmail.com>
2022-10-08 18:10:40 -0300

    modifiqué las constantes

--- src/constantes.js
+++ src/constantes.js
@@ -1,4 +1,4 @@
-export const BACK_URL = 'http://localhost:3001';
-//export const BACK_URL = "https://backpf-production.up.railway.app";
+//export const BACK_URL = 'http://localhost:3001';
+export const BACK_URL = "https://backpf-production.up.railway.app";
 export const FRONT_URL = "http://localhost:3000";
 
