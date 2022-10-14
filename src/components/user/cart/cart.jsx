10cca2ffa328032dd3b718cb8c886c7f864be4ef
agustinteran <agustintern@gmail.com>
2022-10-14 11:16:39 -0300

    centrado de mensaje y descomento meta para que sea responsive

--- src/components/user/cart/cart.jsx
+++ src/components/user/cart/cart.jsx
@@ -64,7 +64,7 @@ function Cart() {
   return (
     !cart.length? (
       <div style={{display:"flex",gap:"12px",flexDirection:"column",width:"100%",justifyContent:"center",height:"200px",alignItems:"center"}}>
-          <h1 style={{fontSize:"23px"}}>No tienes productos en el carrito</h1>
+          <h1 style={{fontSize:"23px",textAlign:"center"}}>No tienes productos en el carrito</h1>
           <button className="datepicker-footer-btn" onClick={() => navigate("/home")}>Ir a agregar</button>
         </div>
     ) :
