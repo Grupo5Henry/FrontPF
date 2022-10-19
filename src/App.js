b00bd9835f45413a7b25353158eadd4a483c5bc1
Gonzalo <zapozapico@gmail.com>
2022-10-19 00:14:32 -0300

    Demo

--- src/App.js
+++ src/App.js
@@ -39,9 +39,7 @@ import Reviews from "./components/Admin/Reviews/Reviews";
 import ForgotPassword from "./components/user/logIn/Forgotpassword";
 import Resetpassword from "./components/user/logIn/Resetpassword";
 import { Landing } from "./components/user/Landing/Landing";
-import { About } from "./components/user/About/About";
-
-
+import { About } from "./components/user/about/about";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
@@ -85,9 +83,9 @@ function App() {
           <NavBar />
           <Routes>
             {/* //// */}
-            <Route exact path="/" element={<Landing/>} />
+            <Route exact path="/" element={<Landing />} />
             <Route exact path="/home" element={<Home />} />
-            <Route exact path="/about" element={<About/>} />
+            <Route exact path="/about" element={<About />} />
 
             <Route exact path="/favorites" element={<Favorites />} />
             <Route exact path="/profile" element={<Profile />} />
@@ -104,10 +102,12 @@ function App() {
             ) : null} */}
             <Route exact path="/home/details" element={<Details />} />
             <Route path="*" element={<Error />} />
-            <Route exact path="/forgotPassword" element={<ForgotPassword/>} />
-            <Route exact path = '/resetPassword/:userName/:token' element = {<Resetpassword/>}/>
-
-
+            <Route exact path="/forgotPassword" element={<ForgotPassword />} />
+            <Route
+              exact
+              path="/resetPassword/:userName/:token"
+              element={<Resetpassword />}
+            />
           </Routes>
           <Footer1 />
         </header>
